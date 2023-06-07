97578ad234b37eb845e0f809abaeb26434bc4869
Татьяна Михайлова <dark.little.finch@gmail.com>
2023-06-07 21:37:13 +0700

    fix: a typo in var questionAnswer was fixed

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -12,16 +12,16 @@ public class Engine {
 
         Scanner scanner = new Scanner(System.in);
 
-        for (String[] questionsAnswer : questionsAnswers) {
-            System.out.println("Question: " + questionsAnswer[questionIndex]);
+        for (String[] questionAnswer : questionsAnswers) {
+            System.out.println("Question: " + questionAnswer[questionIndex]);
             System.out.print("Your answer: ");
             String answer = scanner.next();
 
-            if (answer.equals(questionsAnswer[answerIndex])) {
+            if (answer.equals(questionAnswer[answerIndex])) {
                 System.out.println("Correct!");
             } else {
                 String errorTemplate = "'%s' is wrong answer ;(. Correct answer was '%s'.";
-                System.out.println(String.format(errorTemplate, answer, questionsAnswer[answerIndex]));
+                System.out.println(String.format(errorTemplate, answer, questionAnswer[answerIndex]));
                 System.out.println("Let's try again, " + userName + "!");
                 return;
             }
