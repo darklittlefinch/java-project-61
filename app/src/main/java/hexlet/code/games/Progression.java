acd1face85e5654e08cdecd884f53bdf25ba56ef
Татьяна Михайлова <dark.little.finch@gmail.com>
2023-06-07 20:00:58 +0700

    fix: method getProgression() have more usability now

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -5,32 +5,15 @@ import hexlet.code.Engine;
 
 public class Progression {
     // method generating random arithmetic progression
-    // contains from 5 to 10 numbers
-    public static String[] getProgression() {
-        Random random = new Random();
-
-        // generating a number from 5 to 10
-        // to do it, first generating a number from 5 to 10
-        // then adding to this number 5
-        final var range = 6;
-        final var minNumber = 5;
-        var numbersCount = random.nextInt(range) + minNumber;
+    public static String[] getProgression(int numbersCount, int step, int firstNumber) {
 
         String[] numbers = new String[numbersCount];
 
-        // generating the first number excepting 0 to have more fun :)
-        final var maxNumberOfFirst = 20;
-        var number = random.nextInt(maxNumberOfFirst) + 1;
-        numbers[0] = Integer.toString(number);
+        var numberOfProgression = firstNumber;
 
-        // generating a progression step excepting 0 too
-        // otherwise all the numbers will be equals
-        final var maxNumberOfStep = 10;
-        var step = random.nextInt(maxNumberOfStep) + 1;
-
-        for (var i = 1; i < numbers.length; i++) {
-            numbers[i] = Integer.toString(number + step);
-            number += step;
+        for (var i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.toString(numberOfProgression);
+            numberOfProgression += step;
         }
 
         return numbers;
@@ -43,10 +26,21 @@ public class Progression {
         var questionIndex = 0;
         var answerIndex = 1;
 
+        final var rangeOfCount = 6;
+        final var minCount = 5;
+
+        final var maxSteps = 10;
+
+        final var maxFirstNumber = 20;
+
         Random random = new Random();
 
         for (var i = 0; i < questionsCount; i++) {
-            var progression = getProgression();
+            var numbersCount = random.nextInt(rangeOfCount) + minCount;
+            var step = random.nextInt(maxSteps) + 1;
+            var firstNumber = random.nextInt(maxFirstNumber) + 1;
+
+            var progression = getProgression(numbersCount, step, firstNumber);
 
             // generating a position of number that will we think
             var number = random.nextInt(progression.length);
